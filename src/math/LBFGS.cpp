5a6832e4b9b9dab16a9c44c3741ca7e57f232245
cutesolar <nop@iki.fi>
2015-06-15 11:14:35 +0300

    Bugfixes to LBFGS threading code and optimization code implementation. There need to propagated to BFGS, pLBFGS and pBFGS classes too.

--- src/math/LBFGS.cpp
+++ src/math/LBFGS.cpp
@@ -4,105 +4,102 @@
 #include <iostream>
 #include <list>
 
-
+#include <unistd.h>
 
 
 namespace whiteice
 {
-  namespace math
-  {
-    
-    template <typename T>
+	namespace math
+	{
+
+	template <typename T>
     LBFGS<T>::LBFGS(bool overfit)
     {
-      thread_running = false;
-      sleep_mode = false;
-      solution_converged = false;
-      optimizer_thread = nullptr;
-      
-      this->overfit = overfit;
+		thread_running = false;
+		sleep_mode = false;
+		solution_converged = false;
+		optimizer_thread = nullptr;
+		this->overfit = overfit;
     }
     
     
     template <typename T>
     LBFGS<T>::~LBFGS()
     {
-      thread_mutex.lock();
-      
-      if(thread_running){
-	thread_running = false;
+    	thread_mutex.lock();
 
-	// waits for thread to stop running
-	while(thread_is_running > 0){
-	  std::unique_lock<std::mutex> lock(thread_is_running_mutex);
-	  thread_is_running_cond.wait(lock);
-	}
-	
-	if(optimizer_thread)
-	  delete optimizer_thread;
-	optimizer_thread = nullptr;
-      }
-      
-      thread_mutex.unlock();
+    	if(thread_running){
+    		thread_running = false;
+
+    		// waits for thread to stop running
+    		std::unique_lock<std::mutex> lock(thread_is_running_mutex);
+    		thread_is_running_cond.wait_for(lock, std::chrono::milliseconds(1000)); // 1 second
+    	}
+
+    	if(optimizer_thread)
+    		delete optimizer_thread;
+    	optimizer_thread = nullptr;
+
+    	thread_mutex.unlock();
     }
     
     
     template <typename T>
     bool LBFGS<T>::minimize(vertex<T> x0)
     {
-      thread_mutex.lock();
-      
-      if(thread_running){
-	thread_mutex.unlock();
-	return false;
-      }
-      
-      // calculates initial solution
-      solution_mutex.lock();
-      {
-	heuristics(x0);
-	
-	this->bestx = x0;
-	this->besty = getError(x0);
-	
-	iterations  = 0;
-      }
-      solution_mutex.unlock();
+    	thread_mutex.lock();
 
-      thread_running = true;
-      sleep_mode = false;
-      solution_converged = false;
-      thread_is_running = 0;
-      
-      try{
-	optimizer_thread = new thread(std::bind(&LBFGS<T>::optimizer_loop, this));
-	optimizer_thread->detach();
-      }
-      catch(std::exception& e){
-	thread_running = false;
-	thread_mutex.unlock();
-	return false;
-      }
-      
-      thread_mutex.unlock();
-      
-      return true;
+    	if(thread_running){
+    		thread_mutex.unlock();
+    		return false;
+    	}
+
+    	// calculates initial solution
+    	solution_mutex.lock();
+    	{
+    		heuristics(x0);
+
+    		this->bestx = x0;
+    		this->besty = getError(x0);
+
+    		iterations  = 0;
+    	}
+    	solution_mutex.unlock();
+
+    	thread_running = true;
+    	sleep_mode = false;
+    	solution_converged = false;
+    	thread_is_running = 0;
+
+    	try{
+    		optimizer_thread = new thread(std::bind(&LBFGS<T>::optimizer_loop, this));
+    		optimizer_thread->detach();
+    	}
+    	catch(std::exception& e){
+    		thread_running = false;
+    		thread_mutex.unlock();
+    		return false;
+    	}
+
+    	thread_mutex.unlock();
+
+    	return true;
     }
     
     
     template <typename T>
     bool LBFGS<T>::getSolution(vertex<T>& x, T& y, unsigned int& iterations) const
     {
-      // gets the best found solution
-      solution_mutex.lock();
-      {
-	x = bestx;
-	y = besty;
-	iterations = this->iterations;
-      }
-      solution_mutex.unlock();
-      
-      return true;
+    	// gets the best found solution
+    	solution_mutex.lock();
+    	{
+    		x = bestx;
+    		y = besty;
+    		iterations = this->iterations;
+    	}
+    	solution_mutex.unlock();
+
+    	return true;
     }
     
     
@@ -110,56 +107,54 @@ namespace whiteice
     template <typename T>
     bool LBFGS<T>::continueComputation()
     {
-      sleep_mutex.lock();
-      {
-	sleep_mode = false;
-      }
-      sleep_mutex.unlock();
-      
-      return true;
+    	sleep_mutex.lock();
+    	{
+    		sleep_mode = false;
+    	}
+    	sleep_mutex.unlock();
+
+    	return true;
     }
     
     
     template <typename T>
     bool LBFGS<T>::pauseComputation()
     {
-      sleep_mutex.lock();
-      {
-	sleep_mode = true;
-      }
-      sleep_mutex.unlock();
-      
-      return true;
+    	sleep_mutex.lock();
+    	{
+    		sleep_mode = true;
+    	}
+    	sleep_mutex.unlock();
+
+    	return true;
     }
     
     
     template <typename T>
     bool LBFGS<T>::stopComputation()
     {
-      thread_mutex.lock();
-      
-      if(thread_running == false){
-	thread_mutex.unlock();
-	return false;
-      }
+    	// FIXME threading sychnronization code is BROKEN!
 
-      thread_running = false;
-      
-      {
-	// waits for thread to stop running
-	while(thread_is_running > 0){
-	  std::unique_lock<std::mutex> lk(thread_is_running_mutex);
-	  thread_is_running_cond.wait(lk);
-	}
-      }
+    	thread_mutex.lock();
 
-      if(optimizer_thread)
-	delete optimizer_thread;
-      optimizer_thread = nullptr;
-      
-      thread_mutex.unlock();
-      
-      return true;
+    	if(thread_running == false){
+    		thread_mutex.unlock();
+    		return false;
+    	}
+
+    	thread_running = false;
+
+    	// waits for thread to stop running
+    	std::unique_lock<std::mutex> lock(thread_is_running_mutex);
+    	thread_is_running_cond.wait_for(lock, std::chrono::milliseconds(1000)); // 1 sec
+
+    	if(optimizer_thread)
+    		delete optimizer_thread;
+    	optimizer_thread = nullptr;
+
+    	thread_mutex.unlock();
+
+    	return true;
     }
 
 
@@ -184,64 +179,62 @@ namespace whiteice
 			      const vertex<T>& x,
 			      const vertex<T>& d) const
     {
-      // finds the correct scale first
-      // (exponential search)
+    	// finds the correct scale first
+    	// (exponential search)
 
-      vertex<T> localbestx = x + d;
-      T localbest  = T(1000000000.0f);
-      // T best_alpha = T(1.0f);
-      unsigned int found = 0;
+    	vertex<T> localbestx = x + d;
+    	T localbest  = T(1000000000.0f);
+    	// T best_alpha = T(1.0f);
+    	unsigned int found = 0;
 
-      vertex<T> t;
-      
-      // best_alpha = 0.0f;
-      localbestx = x;
-      localbest = U(localbestx);
-      
-      int k = 0;
-      T alpha = T(1.0f);
+    	vertex<T> t;
 
-      while(found <= 0 && k <= 30){ // min 2**(-30) = 10e-9 step length
+    	// best_alpha = 0.0f;
+    	localbestx = x;
+    	localbest = U(localbestx);
+
+    	int k = 0;
+    	T alpha = T(1.0f);
+
+    	while(found <= 0 && k <= 30){ // min 2**(-30) = 10e-9 step length
+
+    		alpha  = T(::pow(2.0f, k));
+    		T tvalue;
+
+    		t = x + alpha*d;
+    		tvalue = U(t);
+
+    		if(tvalue < localbest){
+    			// if(wolfe_conditions(x, alpha, d))
+    			{
+    				localbest = tvalue;
+    				localbestx = t;
+    				found++;
+    				break;
+    			}
+    		}
+
+    		alpha  = T(1.0f)/alpha;
+
+    		t = x + alpha*d;
+    		tvalue = U(t);
+
+    		if(tvalue < localbest){
+    			// if(wolfe_conditions(x, alpha, d))
+    			{
+    				localbest = tvalue;
+    				localbestx = t;
+    				found++;
+    				break;
+    			}
+    		}
 	
-	alpha  = T(::pow(2.0f, k));
-	T tvalue;
+    		k++;
+    	}
 
-#if 0
-	t = x + alpha*d;
-	tvalue = U(t);
-	
-	if(tvalue < localbest){
-	  // if(wolfe_conditions(x, alpha, d))
-	  {
-	    localbest = tvalue;
-	    localbestx = t;
-	    found++;
-	    break;
-	  }
-	}
-#endif
+    	xn = localbestx;
 
-	alpha  = T(1.0f)/alpha;
-	
-	t = x + alpha*d;
-	tvalue = U(t);
-
-	if(tvalue < localbest){
-	  // if(wolfe_conditions(x, alpha, d))
-	  {
-	    localbest = tvalue;
-	    localbestx = t;
-	    found++;
-	    break;
-	  }
-	}
-	
-	k++;
-      }
-      
-      xn = localbestx;
-      
-      return (found > 0);
+    	return (found > 0);
     }
     
     
@@ -263,223 +256,191 @@ namespace whiteice
     template <typename T>
     void LBFGS<T>::optimizer_loop()
     {
-      
-      vertex<T> d, g; // gradient
-      vertex<T> x(bestx), xn;
-      vertex<T> s, q;
-      
-      T y          = besty;
-      T ratio      = T(1.0f);
-      
-      std::list<T> ratios;
-      bool reset = false;
-      
-      unsigned int M = 35; // history size
-      std::list< vertex<T> > yk;
-      std::list< vertex<T> > sk;
-      std::list< T > rk;
-      
-      thread_is_running++;
-      thread_is_running_cond.notify_all();
-      
-      while(thread_running){
-	try{
-	  
-	  // we keep iterating until we converge (later) or
-	  // the real error starts to increase
-	  if(overfit == false){
-	    ratio = y/besty;
-	    
-	    ratios.push_back(ratio);
-	    while(ratios.size() > 10)
-	      ratios.pop_front();
-	    
-	    T mean_ratio = 1000.0f;
-	    // T inv = 1.0f/ratios.size();
-	    
-	    for(auto& r : ratios)
-	      if(r < mean_ratio) 
-		mean_ratio = r; // min
-	    
-	    // mean_ratio = math::pow(mean_ratio, inv);
-	    
-	    // std::cout << "ratio = " << mean_ratio << std::endl;
-	    
-	    // 10% increase from the minimum found
-	    if(mean_ratio > T(1.10f) && iterations > 10){ 
-	      break;
-	    }
-	  }
+    	vertex<T> d, g; // gradient
+    	vertex<T> x(bestx), xn;
+    	vertex<T> s, q;
 
-	  ////////////////////////////////////////////////////////////
-	  g = Ugrad(x);
-	  
-	  // cancellation point
-	  if(thread_running == false){
-	    thread_is_running_mutex.lock();
-	    thread_is_running--;
-	    thread_is_running_mutex.unlock();
-	    thread_is_running_cond.notify_all();
-	    return;
-	  }
-	  
-	  // d = -H*g; // linsolve(H, d, -g);	  
-	  // calculates aprox hessian product (L-BFGS method)
-	  if(sk.size() > 0){
-	    vertex<T> q(g);
+    	T y          = besty;
+    	T ratio      = T(1.0f);
 
-	    vertex<T> alpha;
-	    vertex<T> beta;
+    	std::list<T> ratios;
+    	bool reset = false;
 
-	    alpha.resize(sk.size());
-	    beta.resize(sk.size());
-	    alpha.zero();
-	    beta.zero();
+    	unsigned int M = 35; // history size
+    	std::list< vertex<T> > yk;
+    	std::list< vertex<T> > sk;
+    	std::list< T > rk;
+
+    	thread_is_running_cond.notify_all();
+      
+    	while(thread_running){
+    		try{
+    			// we keep iterating until we converge (later) or
+    			// the real error starts to increase
+
+    			if(overfit == false){
+    				ratio = y/besty;
+
+    				ratios.push_back(ratio);
+
+    				while(ratios.size() > 10)
+    					ratios.pop_front();
+
+    				T mean_ratio = 1000.0f;
+    				// T inv = 1.0f/ratios.size();
+
+    				for(auto& r : ratios)
+    					if(r < mean_ratio)
+    						mean_ratio = r; // min
+
+    				// mean_ratio = math::pow(mean_ratio, inv);
+    				// std::cout << "ratio = " << mean_ratio << std::endl;
+
+    				// 10% increase from the minimum found
+
+    				if(mean_ratio > T(1.10f) && iterations > 10){
+    					break;
+    				}
+    			}
+
+    			////////////////////////////////////////////////////////////
+    			g = Ugrad(x);
+
+
+    			if(thread_running == false) break; // cancellation point
+
+    			// d = -H*g; // linsolve(H, d, -g);
+    			// calculates aprox hessian product (L-BFGS method)
+
+    			if(sk.size() > 0){
+    				vertex<T> q(g);
+
+    				vertex<T> alpha;
+    				vertex<T> beta;
+
+    				alpha.resize(sk.size());
+    				beta.resize(sk.size());
+    				alpha.zero();
+    				beta.zero();
 	    
-	    {
-	      typename std::list< vertex<T> >::iterator si;
-	      typename std::list< vertex<T> >::iterator yi;
-	      typename std::list< T >::iterator ri;
-	      unsigned int i = 0;
+    				{
+    					typename std::list< vertex<T> >::iterator si;
+    					typename std::list< vertex<T> >::iterator yi;
+    					typename std::list< T >::iterator ri;
+    					unsigned int i = 0;
 	      
-	      for(si = sk.begin(), yi = yk.begin(), ri = rk.begin(), i=0;si!=sk.end();si++,yi++,ri++,i++)
-	      {
-		alpha[i] = (*ri) * ( ((*si)*q)[0] );
-		q = q - alpha[i] * (*yi);
-	      }
-	    }
+    					for(si = sk.begin(), yi = yk.begin(), ri = rk.begin(), i=0;si!=sk.end();si++,yi++,ri++,i++)
+    					{
+    						alpha[i] = (*ri) * ( ((*si)*q)[0] );
+    						q = q - alpha[i] * (*yi);
+    					}
+    				}
 
-	    T Hk = ((*yk.begin()) * (*sk.begin()))[0] / ((*yk.begin())*(*yk.begin()))[0];
 
-	    vertex<T> z = Hk*q;
-	    
-	    {
-	      typename std::list< vertex<T> >::reverse_iterator si;
-	      typename std::list< vertex<T> >::reverse_iterator yi;
-	      typename std::list< T >::reverse_iterator ri;
-	      int i = 0;
+    				T Hk = ((*yk.begin()) * (*sk.begin()))[0] / ((*yk.begin())*(*yk.begin()))[0];
 
-	      const unsigned int m = sk.size();
+    				vertex<T> z = Hk*q;
+
+    				{
+    					typename std::list< vertex<T> >::reverse_iterator si;
+    					typename std::list< vertex<T> >::reverse_iterator yi;
+    					typename std::list< T >::reverse_iterator ri;
+    					int i = 0;
+
+    					const unsigned int m = sk.size();
 		
-	      for(si = sk.rbegin(), yi = yk.rbegin(), ri = rk.rbegin(), i=m-1;si!=sk.rend();si++,yi++,ri++,i--)
-	      {
-		beta[i] = (*ri) * ((*yi) * z)[0];
-		z = z + (*si) * (alpha[i] - beta[i]);
-	      }
-	      
-	    }
+    					for(si = sk.rbegin(), yi = yk.rbegin(), ri = rk.rbegin(), i=m-1;si!=sk.rend();si++,yi++,ri++,i--)
+    					{
+    						beta[i] = (*ri) * ((*yi) * z)[0];
+    						z = z + (*si) * (alpha[i] - beta[i]);
+    					}
 
-	    d = -z;
-	  }
-	  else{
-	    d = -g; // initially just follow the gradient
-	  }
-	  
-	  
-	  // cancellation point
-	  if(thread_running == false){
-	    thread_is_running_mutex.lock();
-	    thread_is_running--;
-	    thread_is_running_mutex.unlock();
-	    thread_is_running_cond.notify_all();
-	    return;
-	  }
-	  
-	  // linear search finds xn = x + alpha*d
-	  // so that U(xn) is minimized
-	  if(linesearch(xn, x, d) == false){
-	    // reset
-	    sk.clear();
-	    yk.clear();
-	    rk.clear();
-	    
-	    if(reset == false){
-	      reset = true;
-	      // continue;
-	    }
-	    else{
-	      // there was reset during the last iteration and 
-	      // we still cannot improve the result
-	      solution_converged = true;
-	      break; // we stop computation as we cannot find better solution
-	    }
-	  }
-	  else{
-	    reset = false;
-	  }
-	  
-	  
-	  heuristics(xn); // heuristically improve xn
-	  
-	  
-	  // cancellation point
-	  if(thread_running == false){
-	    thread_is_running_mutex.lock();
-	    thread_is_running--;
-	    thread_is_running_mutex.unlock();
-	    thread_is_running_cond.notify_all();
-	    return;
-	  }
+    				}
 
+    				d = -z;
+    			}
+    			else{
+    				d = -g; // initially just follow the gradient
+    			}
 	  
-	  // y = U(xn);
-	  y = getError(xn);
-	    
-	  if(y < besty){
-	    std::lock_guard<std::mutex> lock(solution_mutex);
-	    bestx = xn;
-	    besty = y;	    
-	  }
 	  
-	  s = xn - x;
-	  vertex<T> yy = Ugrad(xn) - g; // Ugrad(xn) - Ugrad(x)
-	  T r = T(1.0f)/(s*yy)[0];
+    			// cancellation point
+    			if(thread_running == false) break;
+	  
+    			// linear search finds xn = x + alpha*d
+    			// so that U(xn) is minimized
+    			if(linesearch(xn, x, d) == false){
+    				// reset (we try to just follow gradient instead)
+    				sk.clear();
+    				yk.clear();
+    				rk.clear();
 
-	  sk.push_front(s);
-	  yk.push_front(yy);
-	  rk.push_front(r);
-
-	  while(sk.size() > M) sk.pop_back();
-	  while(yk.size() > M) yk.pop_back();
-	  while(rk.size() > M) rk.pop_back();
-	  
-	  x = xn;
+    				if(reset == false){
+    					reset = true;
+    					iterations++;
+    					continue;
+    				}
+    				else{
+    					// there was reset during the last iteration and
+    					// we still cannot improve the result
+    					solution_converged = true;
+    					break; // we stop computation as we cannot find better solution
+    				}
+    			}
+    			else{
+    				reset = false;
+    			}
 	  
 
-	  // cancellation point
-	  if(thread_running == false){
-	    thread_is_running_mutex.lock();
-	    thread_is_running--;
-	    thread_is_running_mutex.unlock();
-	    thread_is_running_cond.notify_all();
-	    return;
-	  }
-	  
-	  iterations++;
-	}
-	catch(std::exception& e){
-	  std::cout << "ERROR: Unexpected exception: "
-		    << e.what() << std::endl;
-	}
+    			heuristics(xn); // heuristically improve proposed next xn (might break L-BFGS algorithm!!)
 
-	////////////////////////////////////////////////////////////
-	
-	// checks if thread has been ordered to sleep
-	while(sleep_mode){
-	  std::chrono::milliseconds duration(1000);
-	  std::this_thread::sleep_for(duration);
-	}
-      }
-      
-      
-      
-      {
-	thread_is_running_mutex.lock();
-	thread_is_running--;
-	thread_running = false;
-	thread_is_running_mutex.unlock();
-	thread_is_running_cond.notify_all();
-      }
+
+    			// cancellation point
+    			if(thread_running == false) break;
+
+    			// y = U(xn);
+    			y = getError(xn);
+
+    			if(y < besty){
+    				std::lock_guard<std::mutex> lock(solution_mutex);
+    				bestx = xn;
+    				besty = y;
+    			}
+	  
+    			s = xn - x;
+    			vertex<T> yy = Ugrad(xn) - g; // Ugrad(xn) - Ugrad(x)
+    			T r = T(1.0f)/(s*yy)[0];
+
+    			sk.push_front(s);
+    			yk.push_front(yy);
+    			rk.push_front(r);
+
+    			while(sk.size() > M) sk.pop_back();
+    			while(yk.size() > M) yk.pop_back();
+    			while(rk.size() > M) rk.pop_back();
+
+    			x = xn;
+
+    			iterations++;
+    		}
+    		catch(std::exception& e){
+    			std::cout << "ERROR: Unexpected exception: "
+    					<< e.what() << std::endl;
+    			fflush(stdout);
+    		}
+
+    		////////////////////////////////////////////////////////////
+
+    		// checks if thread has been ordered to sleep
+    		while(sleep_mode){
+    			std::chrono::milliseconds duration(1000);
+    			std::this_thread::sleep_for(duration);
+    		}
+    	}
+
+
+    	thread_running = false; // very tricky here, writing false => false or true => false SHOULD BE ALWAYS SAFE without locks
+    	thread_is_running_cond.notify_all(); // waiters have to use wait_for() [timeout milliseconds] as it IS possible to miss notify_all()
     }
     
     
--- src/neuralnetwork/LBFGS_nnetwork.cpp
+++ src/neuralnetwork/LBFGS_nnetwork.cpp
@@ -9,39 +9,47 @@ namespace whiteice
   LBFGS_nnetwork<T>::LBFGS_nnetwork(const nnetwork<T>& nn, const dataset<T>& d, bool overfit, bool negativefeedback) :
     whiteice::math::LBFGS<T>(overfit), net(nn), data(d)
   {
-    this->negativefeedback = negativefeedback;
+	  this->negativefeedback = negativefeedback;
+
+	  // divides data to to training and testing sets
+	  ///////////////////////////////////////////////
+
+	  dtrain = data;
+	  dtest  = data;
     
-    // divides data to to training and testing sets
-    ///////////////////////////////////////////////
+	  dtrain.clearData(0);
+	  dtrain.clearData(1);
+	  dtest.clearData(0);
+	  dtest.clearData(1);
     
-    dtrain = data;
-    dtest  = data;
-    
-    dtrain.clearData(0);
-    dtrain.clearData(1);
-    dtest.clearData(0);
-    dtest.clearData(1);
-    
-    
-    for(unsigned int i=0;i<data.size(0);i++){
-      const unsigned int r = (rand() & 1);
-      
-      if(r == 0){
-	math::vertex<T> in  = data.access(0,i);
-	math::vertex<T> out = data.access(1,i);
+
+	  for(unsigned int i=0;i<data.size(0);i++){
+		  const unsigned int r = (rand() & 1);
+
+		  if(r == 0){
+			  math::vertex<T> in  = data.access(0,i);
+			  math::vertex<T> out = data.access(1,i);
+
+			  dtrain.add(0, in,  true);
+			  dtrain.add(1, out, true);
+		  }
+		  else{
+			  math::vertex<T> in  = data.access(0,i);
+			  math::vertex<T> out = data.access(1,i);
 	
-	dtrain.add(0, in,  true);
-	dtrain.add(1, out, true);
-      }
-      else{
-	math::vertex<T> in  = data.access(0,i);
-	math::vertex<T> out = data.access(1,i);
-	
-	dtest.add(0, in,  true);
-	dtest.add(1, out, true);	    
-      }
-    }
-    
+			  dtest.add(0, in,  true);
+			  dtest.add(1, out, true);
+		  }
+	  }
+
+	  // we cannot never have zero training or testing set size
+	  // in such a small cases (very little data) we just use
+	  // all the data both for training and testing and overfit
+	  if(dtrain.size(0) == 0 || dtest.size(0) == 0){
+		  dtrain = data;
+		  dtest  = data;
+	  }
+
   }
 
   
@@ -102,7 +110,7 @@ namespace whiteice
     {
       T alpha = T(0.01);   // regularizer exp(-0.5*||w||^2) term, w ~ Normal(0,I)
       err = alpha*(x*x);
-      e += x[0];
+      e += err[0];
     }
 #endif
     
@@ -114,41 +122,39 @@ namespace whiteice
   template <typename T>
   math::vertex<T> LBFGS_nnetwork<T>::Ugrad(const math::vertex<T>& x) const
   {
-    whiteice::nnetwork<T> nnet(this->net);
+	  whiteice::nnetwork<T> nnet(this->net);
+
+	  T ninv = T(1.0f); // T(1.0f/data.size(0));
+	  math::vertex<T> sumgrad, grad, err;
+
+	  nnet.importdata(x);
+
+	  sumgrad = x;
+	  sumgrad.zero();
+
+	  for(unsigned int i=0;i<dtrain.size(0);i++){
+		  nnet.input() = dtrain.access(0, i);
+		  nnet.calculate(true);
+		  err = dtrain.access(1,i) - nnet.output();
+
+		  if(nnet.gradient(err, grad) == false){
+			  std::cout << "gradient failed." << std::endl;
+			  assert(0); // FIXME
+		  }
+
+		  sumgrad += ninv*grad;
+	  }
+
+	  {
+		  T alpha = T(0.01f);
+		  sumgrad += alpha*x;
+	  }
+
+	  // TODO: is this really correct gradient to use
+	  // (we want to use: 0,5*SUM e(i)^2 + alpha*w^2
     
-    T ninv = T(1.0f); // T(1.0f/data.size(0));
-    math::vertex<T> sumgrad, grad, err;
 
-    nnet.importdata(x);
-
-    for(unsigned int i=0;i<dtrain.size(0);i++){
-      nnet.input() = dtrain.access(0, i);
-      nnet.calculate(true);
-      err = dtrain.access(1,i) - nnet.output();
-      
-      if(nnet.gradient(err, grad) == false){
-	std::cout << "gradient failed." << std::endl;
-	assert(0); // FIXME
-      }
-      
-      if(i == 0)
-	sumgrad = ninv*grad;
-      else
-	sumgrad += ninv*grad;
-    }
-
-#if 1
-    {
-      T alpha = T(0.01f);
-      sumgrad += alpha*x;
-    }
-#endif
-
-    // TODO: is this really correct gradient to use
-    // (we want to use: 0,5*SUM e(i)^2 + alpha*w^2
-    
-    
-    return (sumgrad);    
+	  return (sumgrad);
   }
   
   
